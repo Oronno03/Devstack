8af1769acdd364dbbdddb6560ae76e0974651807
Oronno03 <iz.oronno@gmail.com>
2026-09-11 17:58:17 +0600

    completed hero section

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,12 +1,30 @@
+import image from "../assets/banner-stack.png";
+
 const Hero = () => {
   return (
     <div className="mx-auto container">
-      <div className="flex justify-between">
-        <div>
-          <h1>Build Your Ideal</h1>
-          <h1>Development Stack</h1>
+      <div className="grid grid-cols-2 items-center">
+        <div className="flex flex-col gap-6 pr-40">
+          <div className="leading-[0.95]">
+            <h1 className="font-main font-extrabold text-[60px] tracking-[-1.5px]">
+              Build Your Ideal
+            </h1>
+            <h1 className="font-main font-extrabold text-[60px] tracking-[-1.5px] bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
+              Development Stack
+            </h1>
+          </div>
+
+          <p className="font-main text-[18px] text-[#475569]">
+            Explore frontend, backend, database, and tooling options, compare
+            them side by side, and put together the stack that fits your next
+            project.
+          </p>
+          <div className="flex gap-3">
+            <button className="w-42.5 rounded-xl px-4 py-3 bg-linear-to-r from-[#F97316] to-[#EC4899] text-[14px] text-white cursor-pointer">Explore Technologies</button>
+            <button className="w-42.5 rounded-xl px-4 py-3 border border-[#E5E7EB] border-solid text-[14px] text-[#374151] cursor-pointer">Learn More</button>
+          </div>
         </div>
-        <img src="a" alt="HERO SECTION IMAGE" />
+        <img src={image} alt="HERO SECTION IMAGE" />
       </div>
     </div>
   );
